58897427b84e399f97f2e0bfd6013b580a1b5a98
Jertzukka <jertzukka@gmail.com>
2020-09-22 10:03:26 +0300

    Added lowres placeholder background with js

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ function Search() {
 
     //let foundItems = arr.filter(callback(element[, index, [array]])[, thisArg])
 }
+
+function upgradeImage(object) {
+    var id = object.id;
+    var target = "html"
+
+    document.getElementById(target).style.backgroundImage = "url(" + object.src + ")";
+}
+
+function is_cached(src) {
+    var image = new Image();
+    image.src = src;
+
+    return image.complete;
+}
